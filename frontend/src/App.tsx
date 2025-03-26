d27e866d7872c6c14c0ecc2d805c75a2f9a753f4
dawangk <85403980+dawangk@users.noreply.github.com>
2025-03-25 20:52:05 -0400

    Develop is Behind Main (#2)
    * frontend setup completed
    
    * Setup backend (#1)
    
    ---------
    
    Co-authored-by: Kevin Lan <kevinlan416@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-blue-500 text-[1000px]"> Hello</div>
+    </>
+  )
+}
+
+export default App
