06f97999cf545dc0e3b490b795b6144cd5f66113
dawangk <85403980+dawangk@users.noreply.github.com>
2025-04-02 10:08:42 -0400

    Update backend/src/routes/portfolioRoutes.ts
    Co-authored-by: Kevin Lan <kevinlan416@gmail.com>

--- backend/src/routes/portfolioRoutes.ts
+++ backend/src/routes/portfolioRoutes.ts
@@ -11,6 +11,6 @@ portfolioRouter.get('/:id', authHandler, getPortfolios);
 portfolioRouter.get('/', authHandler, getPortfolios);
 
 portfolioRouter.put('/:id', authHandler, updatePortfolio);
-portfolioRouter.put('/addFund/:id', authHandler, modifyFunds);
+portfolioRouter.put('/modifyFund/:id', authHandler, modifyFunds);
 
 portfolioRouter.delete('/:id', authHandler, deletePortfolio);
